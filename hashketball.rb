01edb0769c64aa0d55ac3d0040363227775d9d8e
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 22:39:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,6 +76,50 @@ def game_hash
           :steals => ,
           :blocks => ,
           :slam_dunks =>
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
         }
       ]
     }
