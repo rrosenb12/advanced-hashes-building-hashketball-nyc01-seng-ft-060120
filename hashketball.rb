b9ef67d2bb7826c98c34e378f5f38366cd122f59
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-15 22:41:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -89,7 +89,7 @@ def game_hash
           :slam_dunks => 10
         },
         {
-          :player_name => ,
+          :player_name => ,"DeSagna Diop"
           :number => ,
           :shoe => ,
           :points => ,
